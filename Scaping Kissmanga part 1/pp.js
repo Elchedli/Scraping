eaebebbeba9236c73a8498b195a859c4a9a6d44e
shidono <chedli.elloumi@esprit.tn>
2021-02-19 22:56:30 +0100

    Update pp.js

--- Scaping Kissmanga part 1/pp.js
+++ Scaping Kissmanga part 1/pp.js
@@ -6,30 +6,35 @@ const fs = require('fs');
         const page = await browser.newPage();
         const pages = await browser.pages();
         await pages[0].close();
+        var stopper = false;
         var data = fs.readFileSync('file.txt', 'utf8');
         if(data == ""){
             await page.goto("https://konachan.com/post/browse#/sex");
         }else{
             await page.goto(data);
-        }
-        
+        } 
+        await page.exposeFunction("Change", function(){
+            stopper = !stopper
+        });
+        await page.evaluate(()=>{
+            window.addEventListener("keydown", e => {
+                if(e.code == "KeyH"){
+                    Change();
+                };
+            });
+        });
         setTimeout(() => {
             page.keyboard.press(' ');
           },3000);
         setInterval(() => {
-            page.keyboard.press('s');
-            fs.writeFileSync('file.txt',page.url());
+            if(stopper){
+                page.keyboard.press('s');
+                fs.writeFileSync('file.txt',page.url());
+            }
           },3000);
     }catch(e){
         console.log('our error',e);
     }finally{
-        await page.close();
-        console.log('page closed.');
-        await browser.close();
-        fs.close();
+        console.log('Works fine');
     }
 })();
-
-
-
-
