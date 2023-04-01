77d9e166e903a7e8fe24f0431856d33acdc89f21
elchedli <shidonosan@gmail.com>
2023-04-01 02:56:11 +0100

    added first page interaction (partial)

--- search_cheapest_prices.ts
+++ search_cheapest_prices.ts
@@ -16,6 +16,25 @@ const fs = require("fs");
         await page.goto(
             `https://www.tunisair.com.tn/site/publish/content/`
         );
+
+        document.querySelector('select[name=B_LOCATION_1]').value = 'YUL'
+        document.querySelector('select[name=E_LOCATION_1]').value = 'TUN'
+
+        document.querySelector('select[name=ADTPAX]').value = 0
+        document.querySelector('select[name=YTHPAX]').value = 1
+
+
+        const tabMonths = document.querySelectorAll('.month');
+     
+        tabMonths[0].querySelector(`option[innerText=${process.argv[1]} 23`).value
+        tabMonths[0].querySelector('select').value = 
+        tabMonths[0].querySelector(`option[innerText=${process.argv[1]} 23`).value
+
+        const xpath1 = `//option[text()='${process.argv[1]} 23']`;
+        const xpath2 = `//option[text()='${process.argv[2]} 23']`;
+        const optionValue = document.evaluate(xpath, document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue;
+        var iteration1 = 1
+        var iteration2 = 1
     } catch (e) {
         console.log("erreur est : " + e);
     }
