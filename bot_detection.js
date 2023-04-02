23bdde5d4ef4687d3471134d1013c6eec83aa6e9
elchedli <shidonosan@gmail.com>
2023-04-02 03:29:04 +0100

    bypassed first page problems

--- bot_detection.js
+++ bot_detection.js
@@ -1,12 +1,10 @@
-
-
-// user.json tag numberofelements
 const puppeteer = require("puppeteer-extra");
 const stealthPlugin = require('puppeteer-extra-plugin-stealth')
 puppeteer.use(stealthPlugin());
 puppeteer.launch({
     headless: false,
-    executablePath: "/usr/bin/google-chrome",
+    executablePath: "/opt/google/chrome/google-chrome",
+    userDataDir: "/home/shidono/.config/google-chrome/Default",
     defaultViewport: null,
     ignoreDefaultArgs: ["--enable-automation"],
     args: ["--no-sandbox", "--disable-dev-shm-usage"],
@@ -16,19 +14,3 @@ puppeteer.launch({
     await pages[0].close();
     await page.goto('https://bot.sannysoft.com/');
 })
-
-
-// (async function main() {
-//     try {
-//         const browser = await puppeteer.launch({
-//             headless: false,
-//             executablePath: "/usr/bin/google-chrome",
-//             defaultViewport: null,
-//             ignoreDefaultArgs: ["--enable-automation"],
-//             args: ["--no-sandbox", "--disable-dev-shm-usage"],
-//         });
-       
-//     } catch (e) {
-//         console.log("erreur est : " + e);
-//     }
-// })();
--- search_cheapest_prices.js
+++ search_cheapest_prices.js
@@ -1,65 +1,79 @@
-// user.json tag numberofelements
-const puppeteer = require("puppeteer");
-const fs = require("fs");
+const puppeteer = require("puppeteer-extra");
+const stealthPlugin = require("puppeteer-extra-plugin-stealth");
+const fs = require("fs").promises;
+puppeteer.use(stealthPlugin());
 
+const delay = (time) => {
+  return new Promise(function (resolve) {
+    setTimeout(resolve, time);
+  });
+};
 
-function delay(time) {
-    return new Promise(function(resolve) {
-        setTimeout(resolve, time);
-    });
-}
-
-
-(async function main() {
-    try {
-        const browser = await puppeteer.launch({
-            headless: false,
-            executablePath: "/usr/bin/google-chrome",
-            defaultViewport: null,
-            ignoreDefaultArgs: ["--enable-automation"],
-            args: ["--no-sandbox", "--disable-dev-shm-usage"],
-        });
-        const pages = await browser.pages();
-        const page = await browser.newPage();
-        await pages[0].close();
-        await page.goto(
-            `https://www.tunisair.com.tn/site/publish/content/`
-        );
-        // const xpath1 = `//option[text()='${process.argv[1]} 23']`;
-        // const xpath2 = `//option[text()='${process.argv[2]} 23']`;
-        // const goDateOptionValue = document.evaluate(xpath1, document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null);
-        // const returnDateOptionValue = document.evaluate(xpath2, document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null); 
-        // await page.waitForSelector('#blocformsearch');
-        
-        // page.evaluate(() => {
-        //     document.querySelector('select[name=B_LOCATION_1]').value = 'YUL';
-        //     document.querySelector('select[name=E_LOCATION_1]').value = 'TUN';
-        //     document.querySelector('select[name=ADTPAX]').value = '0';
-        //     document.querySelector('select[name=YTHPAX]').value = '1';
-        //     const dates = document.querySelectorAll('.champcal');
-        //     dates[0].value = "13/10/2023";
-        //     dates[1].value = "13/10/2023";
-        // });
-        // await delay(1000);
-        // await page.click('#lignesearch1 > input');
-        await page.waitForSelector('.cash-and-miles-placeholder-container', {timeout: 0});
-        var cookies = await page.cookies();
-        await fs.promises.writeFile(
-            `cookies/tunisair.json`,
-            JSON.stringify(cookies, null, 2)
-        );
-        // const tabMonths = document.querySelectorAll('.month')[0];               
-        // (tabMonths.querySelector('select') as HTMLSelectElement).value = (goDateOptionValue.singleNodeValue as HTMLOptionElement).value;
-
-        
-        // const faresTable = document.querySelectorAll('tbody')[0].querySelectorAll('.calendarPerBound-fare');
-
-        // faresTable.forEach(fareSection => {
-        //     const dateFare = Array(fareSection.querySelector('.calendarPerBound-date-section')?.querySelectorAll('div')).map(dateDiv => dateDiv?['innerText']);
-        // });
-        // var iteration1 = 1
-        // var iteration2 = 1
-    } catch (e) {
-        console.log("erreur est : " + e);
+puppeteer
+  .launch({
+    headless: false,
+    executablePath: "/opt/google/chrome/google-chrome",
+    userDataDir: "/home/shidono/.config/google-chrome/Default",
+    defaultViewport: null,
+    ignoreDefaultArgs: ["--enable-automation"],
+    args: ["--no-sandbox", "--disable-dev-shm-usage"],
+  })
+  .then(async (browser) => {
+    const pages = await browser.pages();
+    const page = await browser.newPage();
+    await pages[0].close();
+    const cookiesString = await fs.readFile("cookies/tunisair.json");
+    if (cookiesString != "") {
+      var cookies = JSON.parse(cookiesString);
+      await page.setCookie(...cookies);
     }
-})();
+    await page.goto(`https://www.tunisair.com.tn/site/publish/content/`);
+
+    // await page.waitForSelector("#blocformsearch");
+    // await page.exposeFunction("delay", delay);
+    // page.evaluate(async () => {
+    //   document.querySelector("select[name=B_LOCATION_1]").value = "YUL";
+    //   await delay(1000);
+    //   document.querySelector("select[name=E_LOCATION_1]").value = "TUN";
+    //   await delay(1000);
+    //   document.querySelector("select[name=ADTPAX]").value = "0";
+    //   await delay(1000);
+    //   document.querySelector("select[name=YTHPAX]").value = "1";
+    //   await delay(1000);
+    //   const dates = await document.querySelectorAll(".champcal");
+    //   dates[0].value = "13/10/2023";
+    //   await delay(1000);
+    //   dates[1].value = "13/12/2023";
+    // });
+    const destination1 = await page.$("select[name=B_LOCATION_1]");
+    await destination1.type("mont");
+    await delay(1000);
+    const destination2 = await page.$("select[name=E_LOCATION_1]");
+    await destination2.type("TUN");
+    await delay(1000);
+    const ageChild = await page.$("select[name=ADTPAX]");
+    await ageChild.type("0");
+    await delay(1000);
+    const ageAdult = await page.$("select[name=YTHPAX]");
+    await ageAdult.type("1");
+    await delay(1000);
+    const dates = await page.$$(".champcal");
+    await dates[0].type("13/10/2023");
+    await delay(1000);
+    await dates[1].type("13/12/2023");
+    // //await delay(1000);
+    await delay(1000);
+    await page.click(".calendarOKButton");
+    await delay(1000);
+    await page.click("#lignesearch1 > input");
+
+    await page.waitForSelector("#tpl3_calendarPerBound", { timeout: 0 });
+    var cookies = await page.cookies();
+    await fs.mkdir("cookies", { recursive: true });
+    await fs.writeFile(
+      "cookies/tunisair.json",
+      JSON.stringify(cookies, null, 2)
+    );
+
+    //browser.close();
+  });
