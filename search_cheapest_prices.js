cb1495f21920fc6f3b316865dce8cc1157294aa4
elchedli <shidonosan@gmail.com>
2023-04-03 00:39:52 +0100

    three functions, getting flightPage data

--- search_cheapest_prices.js
+++ search_cheapest_prices.js
@@ -8,6 +8,53 @@ const delay = (time) => {
     setTimeout(resolve, time);
   });
 };
+const chooseFlightPage = async (page) => {
+  await page.waitForSelector("#blocformsearch");
+  const destination1 = await page.$("select[name=B_LOCATION_1]");
+  // console.log("destination 1 : ", destination1);
+  await destination1.type("mont");
+  await delay(1000);
+  const destination2 = await page.$("select[name=E_LOCATION_1]");
+  await destination2.type("TUN");
+  await delay(1000);
+  const ageChild = await page.$("select[name=ADTPAX]");
+  await ageChild.type("0");
+  await delay(1000);
+  const ageAdult = await page.$("select[name=YTHPAX]");
+  await ageAdult.type("1");
+  await delay(1000);
+  const dates = await page.$$(".champcal");
+  await dates[0].type("13/10/2023");
+  await delay(1000);
+  await dates[1].type("13/12/2023");
+  await delay(1000);
+  await page.click(".calendarOKButton");
+  await delay(1000);
+  await page.click("#lignesearch1 > input");
+};
+
+const checkAndBypass = async (page) => {
+  await page.waitForSelector("#tpl3_calendarPerBound", { timeout: 0 });
+  var cookies = await page.cookies();
+  await fs.mkdir("cookies", { recursive: true });
+  await fs.writeFile("cookies/tunisair.json", JSON.stringify(cookies, null, 2));
+};
+
+const flightPage = async (page) => {
+  const result = await page.evaluate(() => {
+    const faresTables = document.querySelectorAll(
+      "tbody:nth-of-type(1) .calendarPerBound-fare"
+    );
+    return Array.from(faresTables).map((fareSection) => {
+      const dateFare = Array.from(
+        fareSection.querySelectorAll(".calendarPerBound-date-section > div")
+      ).map((div) => div.innerText);
+      const price = fareSection.querySelector(".price-amount");
+      return { date: dateFare, price: price ? price.innerText : null };
+    });
+  });
+  return result;
+};
 
 puppeteer
   .launch({
@@ -29,51 +76,9 @@ puppeteer
     }
     await page.goto(`https://www.tunisair.com.tn/site/publish/content/`);
 
-    // await page.waitForSelector("#blocformsearch");
-    // await page.exposeFunction("delay", delay);
-    // page.evaluate(async () => {
-    //   document.querySelector("select[name=B_LOCATION_1]").value = "YUL";
-    //   await delay(1000);
-    //   document.querySelector("select[name=E_LOCATION_1]").value = "TUN";
-    //   await delay(1000);
-    //   document.querySelector("select[name=ADTPAX]").value = "0";
-    //   await delay(1000);
-    //   document.querySelector("select[name=YTHPAX]").value = "1";
-    //   await delay(1000);
-    //   const dates = await document.querySelectorAll(".champcal");
-    //   dates[0].value = "13/10/2023";
-    //   await delay(1000);
-    //   dates[1].value = "13/12/2023";
-    // });
-    const destination1 = await page.$("select[name=B_LOCATION_1]");
-    await destination1.type("mont");
-    await delay(1000);
-    const destination2 = await page.$("select[name=E_LOCATION_1]");
-    await destination2.type("TUN");
-    await delay(1000);
-    const ageChild = await page.$("select[name=ADTPAX]");
-    await ageChild.type("0");
-    await delay(1000);
-    const ageAdult = await page.$("select[name=YTHPAX]");
-    await ageAdult.type("1");
-    await delay(1000);
-    const dates = await page.$$(".champcal");
-    await dates[0].type("13/10/2023");
-    await delay(1000);
-    await dates[1].type("13/12/2023");
-    // //await delay(1000);
-    await delay(1000);
-    await page.click(".calendarOKButton");
-    await delay(1000);
-    await page.click("#lignesearch1 > input");
-
-    await page.waitForSelector("#tpl3_calendarPerBound", { timeout: 0 });
-    var cookies = await page.cookies();
-    await fs.mkdir("cookies", { recursive: true });
-    await fs.writeFile(
-      "cookies/tunisair.json",
-      JSON.stringify(cookies, null, 2)
-    );
-
+    await chooseFlightPage(page);
+    await checkAndBypass(page);
+    const data = await flightPage(page);
+    console.log(data);
     //browser.close();
   });
