b2dbbe1e97a0afed05e825a779c6568bee33012f
elchedli <shidonosan@gmail.com>
2023-04-04 01:41:26 +0100

    working perfectly but no features

---
+++ readme.md
@@ -0,0 +1,2 @@
+to start the code you need to type in the console : node search_cheapest_prices.js firstDate secondDate in (jj/dd/yyyy) format.
+Example : node search_cheapest_prices.js 15/5/2023 15/12/2023
--- search_cheapest_prices.js
+++ search_cheapest_prices.js
@@ -9,41 +9,27 @@ const delay = (time) => {
   });
 };
 
-// const lastday = (y, m) => {
-//   return new Date(y, m + 1, 0).getDate();
-// };
-
-Date.prototype.addDays = (days) => {
-  var date = new Date(this.valueOf());
-  date.setDate(date.getDate() + days);
-  return date;
+Date.prototype.addDays = function (days) {
+  this.setDate(this.getDate() + days);
+  return this;
 };
 
 const chooseFlightPage = async (page, datePlayer) => {
-  console.log("date : ", datePlayer);
   await page.waitForSelector("#blocformsearch");
   const destination1 = await page.$("select[name=B_LOCATION_1]");
   await destination1.type("mont");
-  await delay(1000);
   const destination2 = await page.$("select[name=E_LOCATION_1]");
   await destination2.type("TUN");
-  await delay(1000);
   const ageChild = await page.$("select[name=ADTPAX]");
   await ageChild.type("0");
-  await delay(1000);
   const ageAdult = await page.$("select[name=YTHPAX]");
   await ageAdult.type("1");
-  await delay(1000);
   const dates = await page.$$(".champcal");
   //there is start and end date for destination
   await dates[0].type(datePlayer);
+  await delay(500);
   await dates[1].type(datePlayer);
-  // dates.forEach(async (date) => {
-  //   await date.type(datePlayer);
-  // });
-  await delay(1000);
   await page.click(".calendarOKButton");
-  await delay(1000);
   await page.click("#lignesearch1 > input");
 };
 
@@ -58,12 +44,16 @@ const flightPage = async (page) => {
     const faresTables = document.querySelectorAll(
       "tbody:nth-of-type(1) .calendarPerBound-fare"
     );
-    return Array.from(faresTables).map((fareSection) => {
+    return Array.from(faresTables).map((fareSection, index) => {
       const dateFare = Array.from(
         fareSection.querySelectorAll(".calendarPerBound-date-section > div")
       ).map((div) => div.innerText);
       const price = fareSection.querySelector(".price-amount");
-      return { date: dateFare, price: price ? price.innerText : null };
+      return {
+        type: index < 7 ? "aller" : "retour",
+        date: dateFare.toString().replaceAll(",", ""),
+        price: price ? price.innerText : null,
+      };
     });
   });
   return result;
@@ -95,24 +85,23 @@ puppeteer
       await page.setCookie(...cookies);
     }
     await fs.mkdir("cookies", { recursive: true });
-    // await page.goto(`https://www.tunisair.com.tn/site/publish/content/`);
-    var date = new Date(year, month, day);
+    var date = new Date(year, month - 1, day);
     const dateEndTime = new Date(yearEnd, monthEnd, dayEnd).getTime();
-    // console.log([day,month,year].join('/'))
     var dataCollection = [];
     while (date.getTime() < dateEndTime) {
-      const dateFormatted = `${date.getDate()}/${date.getMonth()}/${date.getFullYear()}`;
+      const dateFormatted = `${("0" + date.getDate()).slice(-2)}/${(
+        "0" +
+        (parseInt(date.getMonth()) + 1)
+      ).slice(-2)}/${date.getFullYear()}`;
       await page.goto(`https://www.tunisair.com.tn/site/publish/content/`);
       await chooseFlightPage(page, dateFormatted);
       await checkAndBypass(page);
       const data = await flightPage(page, dateFormatted);
-      concat(dataCollection, data);
-      date.addDays(6);
+      dataCollection = dataCollection.concat(data);
+      date.addDays(7);
+      console.log("date : ", dateFormatted);
     }
-    // console.log(process.argv[2], process.argv[3]);
-    // console.log(day, month, year, "  and ", typeof day, typeof month);
-    // var formattedDate = ;
-    // console.log(formattedDate);
+    dataCollection = dataCollection.filter((data) => data.price != null);
     console.log(dataCollection);
-    //browser.close();
+    browser.close();
   });
